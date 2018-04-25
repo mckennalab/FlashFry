0dcbe54e505fbce25be2fa4e50294ec9836a744b
Aaron McKenna <aaronatwpi@gmail.com>
2018-04-25 12:30:01 -0700

    run the timing collection pipeline

--- paper/run_timing_collection_full.py
+++ paper/run_timing_collection_full.py
@@ -1,9 +1,9 @@
 from subprocess import call
 
-testrange = ["1","10", "100", "1000", "10000","100000"]
+testrange = ["1","10", "100", "1000"]
 mismatches = ["3","4","5"]
 
-score_output = open("/home/ec2-user/ff_git/paper/tools/runtimes.txt","w")
+score_output = open("/home/ec2-user/ff_git/paper/tools/runtimes_full.txt","w")
 
 
 def get_wall_time(inputFile):
@@ -44,5 +44,7 @@ for i in testrange:
         score_output.write("BWA_ALN\t" + i + "\t" + mismatch + "\t" + get_wall_time("bwaaln" + i + "_i" + mismatch + ".stderr") + "\t" + get_mem_usage("bwaaln" + i + "_i" + mismatch + ".stderr") + "\n")
         score_output.write("BWA_SAMSE\t" + i + "\t" + mismatch + "\t" + get_wall_time("samse" + i + "_i" + mismatch + ".stderr") + "\t" + get_mem_usage("samse" + i + "_i" + mismatch + ".stderr") + "\n")
         score_output.write("FLASHFRY\t" + i + "\t" + mismatch + "\t" + get_wall_time("flashfry" + i + "_i" + mismatch + ".stderr") + "\t" + get_mem_usage("flashfry" + i + "_i" + mismatch + ".stderr") + "\n")
+        score_output.write("CASOFF\t" + i + "\t" + mismatch + "\t" + get_wall_time("casoff" + i + "_i" + mismatch + ".stderr") + "\t" + get_mem_usage("casoff" + i + "_i" + mismatch + ".stderr") + "\n")
+        score_output.write("CRISPSEEK\t" + i + "\t" + mismatch + "\t" + get_wall_time("crisprSeek" + i + "_i" + mismatch + ".stderr") + "\t" + get_mem_usage("crisprSeek" + i + "_i" + mismatch + ".stderr") + "\n")
 
 score_output.close()
